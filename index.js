2632e650d5dc23fa152f4f2e7736cc9d3e3d48be
pleycpl <h4rvey@protonmail.com>
2018-03-10 13:26:49 +0000

    listening all messages in telegram chat

---
+++ index.js
@@ -0,0 +1,27 @@
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('./config.js');
+const util = require('./util.js');
+
+const bot = new TelegramBot(config.TelegramBotToken, {polling: true});
+console.log(`Test Bot started, ${config.TelegramBotUri}`)
+
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+	const message = msg.text.toString();
+	
+	if (message.includes(config.TelegramBotName)) {
+		let links = util.ParseLinksInText(message)
+		bot.sendMessage(chatId, `Okey, I added links. ${links}`)
+	}
+});
+
+// Error Handling
+bot.on('polling_error', (error) => {
+	console.log("Polling Error")
+	console.log(error)
+});
+
+bot.on('webhook_error', (error) => {
+	console.log("Webhook Error")
+	console.log(error)
+});
